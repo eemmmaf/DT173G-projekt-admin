0888d81d7a4886132a8b86a6f7901096542b8dee
Emma Forslund <emfo2102@student.miun.se>
2022-06-03 20:00:18 +0200

    Lagt till kontaktuppgifter och en del med aktuella händelser

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /*
  * @Author: Emma Forslund - emfo2102 
  * @Date: 2022-06-01 15:22:08 
- * @Last Modified by:   Emma Forslund - emfo2102 
- * @Last Modified time: 2022-06-01 15:22:08 
+ * @Last Modified by: Emma Forslund - emfo2102
+ * @Last Modified time: 2022-06-03 13:26:20
  */
 
 include('includes/header.php');
@@ -13,17 +13,37 @@ if (!isset($_SESSION["admin"])) {
 }
 ?>
 <main>
-    <div class="start-flex">
-        <div id="bookings">
-            <h2>Nya bokningar</h2>
-        </div>
-        <div id="contact">
+    <section class="index-section">
+        <h1>
+            Administrationssidan för anställda på Trattoria Romantico
+        </h1>
+        <p>På denna webbplats kan du administrera restaurangens meny och bokningar. Det är möjligt att ta bort, ändra och lägga till bokningar och maträtter/dryck. </p>
+        <article id="contact">
             <h2>Kontaktuppgifter</h2>
-
-        </div>
-    </div>
+            <p><b>För frågor om bokningar och menyn, ring eller maila restaurangansvarige Emma</b></p>
+            <ul>
+                <li>076 101 54 74</li>
+                <li><a href="mailto:emfo2102@student.miun.se">emfo2102@student.miun.se</a> </li>
+            </ul>
+            <br><br>
+            <p><b>För tekniska frågor, ring restaurangens teknikansvarige Emma</b></p>
+            <ul>
+                <li>076 101 54 74</li>
+            </ul>
+            </div>
+    </section>
+    <section id="aktuellt">
+        <h2>Aktuellt</h2>
+        <article class="aktuellt">
+            <h3>AW på Boule bar</h3>
+            <img src="images/boule.jpg" alt="man står i en boule-bana">
+            <p>Äntligen är sommaren här! För att fira in sommaren ses vi på Boule bar i Hagaparken. Vi bjuder på mat, dryck och obegränsat med spel. Vi ses klockan 17:00! <br><br>
+                /Emma </p>
+        </article>
+    </section>
 </main>
 <footer>
+    <p>Emma Forslund | Trattoria Romantico | emfo2102@student.miun.se</p>
 </footer>
 </body>
 
